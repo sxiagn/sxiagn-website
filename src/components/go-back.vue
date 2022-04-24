361a6cf502afe0e9d01c83ce6b7b8167f0e1fe7c
sxiagn <936094205@qq.com>
2022-04-24 21:57:44 +0800

    添加返回上一路由组件

---
+++ src/components/go-back.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="go-back" @click="$router.back()">
+    <el-icon size="20" color="#fff"><arrow-left-bold /></el-icon>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.go-back {
+  background-color: #67c23a;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  color: #fff;
+  text-align: center;
+  line-height: 30px;
+  font-size: 14px;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+
+  &:hover {
+    background-color: #85ce61;
+  }
+}
+</style>
